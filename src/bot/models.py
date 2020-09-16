7644be58e9be8bdc0b5ee2cb187ae5c4ba01e612
strpc <renat.kruchinskij@gmail.com>
2020-09-17 02:17:45 +0300

    добавил модели бота

--- src/bot/models.py
+++ src/bot/models.py
@@ -1,3 +1,63 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """Модель юзера"""
+    chat_id = models.BigIntegerField('Chat ID', unique=True, null=False, blank=False)
+    first_name = models.CharField('Имя', max_length=150, null=True, blank=True)
+    last_name = models.CharField('Фамилия', max_length=150, null=True, blank=True)
+    username = models.CharField('Username', max_length=150, null=True, blank=True)
+    register: datetime = models.DateTimeField('Зарегестрировался', auto_now_add=True)
+    active = models.BooleanField('Активный', default=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} @{self.username} ' \
+               f'{self.register.strftime("%d.%m.%Y %H:%M")}'
+
+    class Meta:
+        db_table = 'bot_users'
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+
+class RSS(models.Model):
+    """Фиды пользователей"""
+    chat_id: User = models.ForeignKey(
+        User, to_field='chat_id', verbose_name='Пользователь', on_delete=models.CASCADE
+    )
+    url = models.CharField('URL RSS', max_length=2000, null=False, blank=False)
+    added: datetime = models.DateTimeField('Добавлено', auto_now_add=True)
+    active = models.BooleanField('Активная', default=True, blank=False, null=False)
+
+    def __str__(self):
+        return f'{self.chat_id.first_name} {self.chat_id.last_name} @{self.chat_id.username} ' \
+               f'{self.url} {self.added.strftime("%d.%m.%Y %H:%M")}'
+
+    class Meta:
+        db_table = 'bot_users_rss'
+        verbose_name = 'RSS пользователя'
+        verbose_name_plural = 'RSS пользователей'
+
+
+class Article(models.Model):
+    """Статьи"""
+    chat_id: User = models.ForeignKey(
+        User, to_field='chat_id', verbose_name='Пользователь', on_delete=models.CASCADE
+    )
+    url_rss: RSS = models.ForeignKey(RSS, verbose_name='URL RSS', on_delete=models.CASCADE)
+    url_article = models.TextField('Ссылка на статью', blank=False, null=False)
+    title = models.CharField('Заголовок статьи', max_length=1000, blank=True, null=True)
+    text = models.CharField('Текст статьи', max_length=2000, blank=True, null=True)
+    sended = models.BooleanField('Активная', default=True, blank=False, null=False)
+    added: datetime = models.DateTimeField('Добавлено', auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.chat_id.first_name} {self.chat_id.last_name} @{self.chat_id.username} ' \
+               f'{self.url_rss.url} {self.added.strftime("%d.%m.%Y %H:%M")} {self.sended}'
+
+    class Meta:
+        db_table = 'bot_article'
+        verbose_name = 'Статью'
+        verbose_name_plural = 'Статьи'
