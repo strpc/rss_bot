b1e4dfe3fc9485459013e1d7f5f29ed2b7bf02a5
strpc <renat.kruchinskij@gmail.com>
2020-09-17 02:20:38 +0300

    добавил класс с сообщениями из бд, добавил регулярки для определения команд бота

--- src/core/schemas/update.py
+++ src/core/schemas/update.py
@@ -1,9 +1,13 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from enum import Enum
+import re
 
-from src.core.utils import from_timestamp
+# from src.core.utils import from_timestamp
 from src.core.requests.database import Database
 from src.core.requests.api import Request
+
+
+command_compile = re.compile(r'(^|\s)\/\b[a-zA-Z]+\b')
 
 
 class TypeUpdate(str, Enum):
@@ -13,48 +17,50 @@ class TypeUpdate(str, Enum):
 
 
 class BaseMessage(ABC):
+    @abstractmethod
     def __init__(self, **kwargs):
         _message = kwargs.get('message')
         self.chat_id = _message.get('chat').get('id')
         self.first_name = _message.get('chat').get('first_name')
         self.last_name = _message.get('chat').get('last_name')
         self.username = _message.get('chat').get('username')
-        self.date = from_timestamp(_message.get('date'))
+        # self.date = from_timestamp(_message.get('date'))
         self.text = ''
         self.command = ''
         self.type_update = TypeUpdate.undefined.name
         self.new_user = False
-        self.init_user()
 
-    def init_user(self):
-        # if user is None:
-        self.new_user = True
+    def _init_user(self) -> bool:
+        self.new_user = False if self.database.init_user() else True
+        return self.new_user
 
     @property
     def database(self):
-        return Database()
+        return Database(chat_id=self.chat_id)
 
     @property
     def request(self):
-        return Request()
+        return Request(chat_id=self.chat_id)
 
 
 class BotCommand(BaseMessage):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.command = kwargs.get('message').get('text')
+        _text = kwargs.get('message').get('text')
+        self.command = re.search(command_compile, _text)[0] \
+            if re.search(command_compile, _text) else None
+        self.text = re.sub(command_compile, '', _text)
+        self.command_raw = self.command[1:] if self.command else None
         self.type_update = TypeUpdate.command.name
-        # if self.new_user:
-        self.register_user()
-
-        # если не новый и команда /start - raise ошибка
 
     def register_user(self):
-        # if user not in database
-        pass
+        self.database.register_user(self)
 
-    def __repr__(self):
-        return f'Command: {self.command}. {self.username}, {self.chat_id}, {self.date}'
+    def init_user(self):
+        return self._init_user()
+
+    def __repr__(self) -> str:
+        return f'Command: {self.command}. @{self.username}, {self.chat_id}'
 
 
 class Message(BaseMessage):
@@ -64,5 +70,9 @@ class Message(BaseMessage):
         self.type_update = TypeUpdate.message.name
 
     def __repr__(self):
-        return f'Message: {self.text}. {self.username}, {self.chat_id}, {self.date}'
+        return f'Message: {self.text}. @{self.username}, {self.chat_id}'
 
+
+class FromDB(BaseMessage):
+    def __init__(self, chat_id: int):
+        self.chat_id = chat_id
