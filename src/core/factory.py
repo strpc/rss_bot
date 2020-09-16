ab5cdc6fe8e655f45bf799568e8a86174e2924b7
strpc <renat.kruchinskij@gmail.com>
2020-09-16 12:14:05 +0300

    доделал фабрику

---
+++ src/core/factory.py
@@ -0,0 +1,29 @@
+from typing import Dict
+
+from src.core.models.update import Message, BotCommand
+
+
+class ObjectFactory:
+    def __init__(self, list_obj: Dict):
+        self._builders = list_obj
+
+    def register_message(self, message: Dict):
+        type_msg = 'message'
+        entitles = message.get('message').get('entities')
+        if entitles:
+            for entitle in entitles:
+                for k, v in entitle.items():
+                    if v in self._builders.keys():
+                        type_msg = v
+
+        obj = self._builders.get(type_msg)
+        return obj(**message)
+
+
+type_obj = {
+    'bot_command': BotCommand,
+    'message': Message,
+}
+
+
+Factory = ObjectFactory(type_obj)
