f78fafe06037e83507cb2e8a67d924f1f7fcb367
strpc <renat.kruchinskij@gmail.com>
2020-09-17 02:21:17 +0300

    сделал фабрику на ровном месте

--- src/bot/views.py
+++ src/bot/views.py
@@ -1,12 +1,17 @@
+from pprint import pprint
+from typing import Union
 import logging
 import json
-from pprint import pprint
 
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse, HttpRequest
 
+from src.core.factory import Factory
+from src.core.schemas.update import BaseMessage, BotCommand, TypeUpdate
+from src.core.handlers import CommandHandler
 
-LOGGER = logging.getLogger(__name__)
+
+logger = logging.getLogger(__name__)
 
 
 @csrf_exempt
@@ -14,9 +19,16 @@ def token_handler(request: HttpRequest) -> JsonResponse:
     if request.method == "POST":
         body_unicode = request.body.decode('utf-8')
         body = json.loads(body_unicode)
-        LOGGER.info(body)
+        logger.info(body)
         pprint(body)
-        # message: Message = Factory.register_message(body)
-        # handlers.process_message(message)
-        response = JsonResponse({'success': 'true'}, status=200)
-        return response
+        message: Union[BaseMessage, BotCommand] = Factory.register_message(body)
+
+        if message.type_update == TypeUpdate.command.name and \
+                message.command_raw in CommandHandler.__dict__.keys():
+            CommandHandler.__dict__[message.command_raw].__get__(CommandHandler)(message)
+
+        elif message.type_update == TypeUpdate.message.name:
+            pass  # пришло обычное сообщение. нужно сделать заглушку.
+
+        return JsonResponse({'success': 'true'}, status=200)
+
