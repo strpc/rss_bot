a73eb007d0cd9431979e882f00316507655ad6b9
strpc <renat.kruchinskij@gmail.com>
2020-09-17 02:20:57 +0300

    добавил начало функции для парсинга урлов

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,8 +1,12 @@
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 
 
 def from_timestamp(unixtime: Optional[int] = None) -> Optional[datetime]:
     if unixtime is None:
         return None
     return datetime.fromtimestamp(unixtime)
+
+
+def list_urls(urls: str) -> List[str]:
+    pass
