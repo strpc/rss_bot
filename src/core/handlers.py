a265132b276c36b77efce54d78ec3866b8f69ce7
strpc <renat.kruchinskij@gmail.com>
2020-09-18 21:42:43 +0300

    добавил обработку модели с урлами

--- src/core/handlers.py
+++ src/core/handlers.py
@@ -1,7 +1,8 @@
 import logging
+from pprint import pprint
 
 from src.core.schemas.update import BotCommand
-from src.core.utils import list_urls
+from src.core.schemas.rss import ListUrls
 
 
 logger = logging.getLogger(__name__)
@@ -13,13 +14,17 @@ class CommandHandler:
         message.init_user()
         if message.new_user:
             message.register_user()
-        message.request.send_message('здарова дятел')
+            message.request.send_message('здарова дятел')
+            # первый старт/рестарт пользователем
+        # повторный /start без остановки бота
+        print('hello method start')
 
     @staticmethod
     def add_feed(message: BotCommand):
-        urls = list_urls(message.text.replace('/add_feed', ''))
-
-        pass  # INSERT OR IGNORE
+        list_urls = ListUrls(list_urls=message.text)
+        # todo: сделать валидацию урлов
+        message.database.add_feed(list_urls)
+        # INSERT OR IGNORE
 
     @staticmethod
     def delete_feed(message: BotCommand):
@@ -28,3 +33,10 @@ class CommandHandler:
     @staticmethod
     def list_feed(message: BotCommand):
         pass
+
+
+# {
+#   "ok": false,
+#   "error_code": 403,
+#   "description": "Forbidden: bot was blocked by the user"
+# }
