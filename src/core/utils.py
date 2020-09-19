b145d49ffe85fe066cdce2e63a5000827915795a
strpc <renat.kruchinskij@gmail.com>
2020-09-19 16:37:13 +0300

    добавил функцию создания хэша

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from typing import Optional, List
+import base64
 
 
 def from_timestamp(unixtime: Optional[int] = None) -> Optional[datetime]:
@@ -8,5 +9,14 @@ def from_timestamp(unixtime: Optional[int] = None) -> Optional[datetime]:
     return datetime.fromtimestamp(unixtime)
 
 
-def list_urls(urls: str) -> List[str]:
-    pass
+def make_hash(*args) -> str:
+    hash_ = ''
+    for i in args:
+        if not isinstance(i, (str, int)):
+            continue
+        hash_ += str(i)
+    return base64.b64encode(hash_.encode()).decode()
+
+
+def make_str_urls(urls: List) -> str:
+    return '\n'.join(urls)
