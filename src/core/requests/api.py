49a6dcb39ca0a6e5c6bf711a5698301f09dab263
strpc <renat.kruchinskij@gmail.com>
2020-09-19 16:35:31 +0300

    добавил parse_mode и disable_preview для отправки сообщений

--- src/core/requests/api.py
+++ src/core/requests/api.py
@@ -14,6 +14,8 @@ REQUEST_URL = f"{API_BASE_URL}/bot{BOT_TOKEN}/"
 
 
 class Client(ABC):
+    """Обертка над запросами в API"""
+
     @abstractmethod
     def __init__(self):
         pass
@@ -39,13 +41,21 @@ class Client(ABC):
         return r
 
     def post(
-            self, method: str, params=None, body=None, data=None, attempt=ATTEMPT_REQUEST
+            self,
+            method: str,
+            params=None,
+            body=None,
+            data=None,
+            attempt=ATTEMPT_REQUEST
     ) -> httpx.Response:
         """Обертка над post-запросом"""
         r = httpx.post(
             url=REQUEST_URL + self.__method_edit(method), json=body, params=params, data=data
         )
-        if r.status_code != 200 and attempt:
+        if r.status_code == 200:
+            return r
+
+        elif r.status_code != 200 and attempt:
             sleep(DELAY_REQUEST)
             attempt -= 1
             self.post(method, params, body, data, attempt)
@@ -56,7 +66,7 @@ class Client(ABC):
                 'response: %s', REQUEST_URL + method, params, body, bool(data), r.status_code,
                 r.json()
             )
-        return r
+            return r
 
 
 class Request(Client):
@@ -64,11 +74,20 @@ class Request(Client):
         super().__init__()
         self.chat_id = chat_id
 
-    def send_message(self, text: str):
+    def send_message(
+            self,
+            text: str,
+            parse_mode: str = None,
+            disable_web_page_preview: bool = False
+    ) -> httpx.Response:
         """Отправка сообщений пользователю"""
         method = "sendMessage"
         body = {
             "chat_id": self.chat_id,
             "text": text
         }
-        self.post(method=method, body=body)
+        if parse_mode:
+            body['parse_mode'] = parse_mode
+        if disable_web_page_preview:
+            body['disable_web_page_preview'] = True
+        return self.post(method=method, body=body)
