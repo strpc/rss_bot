37a878d9fabfb59786c4918a779bbee2269732e1
strpc <renat.kruchinskij@gmail.com>
2020-09-19 16:36:13 +0300

    добавил удаление урла

--- src/core/handlers.py
+++ src/core/handlers.py
@@ -1,8 +1,13 @@
 import logging
 from pprint import pprint
+from random import choice
+
+from pydantic import ValidationError
 
 from src.core.schemas.update import BotCommand
-from src.core.schemas.rss import ListUrls
+from src.core.schemas.rss import ListUrls, UrlFeed
+from src.core.utils import make_str_urls
+from src.project.settings import parse_mode_markdown
 
 
 logger = logging.getLogger(__name__)
@@ -23,16 +28,39 @@ class CommandHandler:
     def add_feed(message: BotCommand):
         list_urls = ListUrls(list_urls=message.text)
         message.add_feed(list_urls)
-        message.send_message('Ваши урлы добавлены. Скоро будете '
-                                     'получать сообщения')
 
-    @staticmethod
-    def delete_feed(message: BotCommand):
-        pass
+        message.send_message(f"URLs: {make_str_urls(list_urls)} \n has "
+                             f"beed added to your feed.")
 
     @staticmethod
     def list_feed(message: BotCommand):
-        pass
+        list_dicts_urls = message.list_feed()
+        list_str_urls = [i.get('url') for i in list_dicts_urls]
+        msg = f'You are subscribed to:\n{make_str_urls(list_str_urls)}'
+        message.send_message(msg)
+
+    @staticmethod
+    def delete_feed(message: BotCommand):
+        if not message.text:
+            list_dicts_urls = message.list_feed()
+            text = 'For unsubscribe send:\n'
+            for item in range(0, len(list_dicts_urls)):
+                url = choice(list_dicts_urls)
+                text += f"`/delete_feed {url['url']}`\n"
+                list_dicts_urls.remove(url)
+            message.send_message(text, parse_mode_markdown, True)
+        else:
+            try:
+                url = UrlFeed(url_feed=message.text)
+            except ValidationError:
+                message.send_message('Bad url')
+                return
+            if message.delete_feed(str(url)):
+                message.send_message(
+                    f'URL {str(url)} has been removed from your feed', disable_web_page_preview=True
+                )
+
+
 
 
 # {
