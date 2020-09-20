584f2cbdd83543e508533579eb51f77e9cff0bf7
strpc <renat.kruchinskij@gmail.com>
2020-09-20 21:02:04 +0300

    в add_feed добавил проверку на длину списка. если один - сообщение одного формата отправляется. если много - другой формат. доделал /delete_feed

--- src/core/handlers.py
+++ src/core/handlers.py
@@ -1,13 +1,13 @@
 import logging
+import random
 from pprint import pprint
-from random import choice
 
 from pydantic import ValidationError
 
 from src.core.schemas.update import BotCommand
 from src.core.schemas.rss import ListUrls, UrlFeed
 from src.core.utils import make_str_urls
-from src.project.settings import parse_mode_markdown
+from src.project.settings import PARSE_MODE_MARKDOWN
 
 
 logger = logging.getLogger(__name__)
@@ -28,16 +28,19 @@ class CommandHandler:
     def add_feed(message: BotCommand):
         list_urls = ListUrls(list_urls=message.text)
         message.add_feed(list_urls)
-
-        message.send_message(f"URLs: {make_str_urls(list_urls)} \n has "
-                             f"beed added to your feed.")
+        text = 'твои ссылки - шляпа'  # заглушка, если вдруг пришла одна ссылка и она не rss
+        if len(list_urls) > 1:
+            text = f"URLs: {make_str_urls(list_urls)} \nhas beed added to your feed."
+        elif len(list_urls) == 1:
+            text = f"URL: {make_str_urls(list_urls)} \nhas beed added to your feed."
+        message.send_message(text, disable_web_page_preview=True)
 
     @staticmethod
     def list_feed(message: BotCommand):
         list_dicts_urls = message.list_feed()
         list_str_urls = [i.get('url') for i in list_dicts_urls]
         msg = f'You are subscribed to:\n{make_str_urls(list_str_urls)}'
-        message.send_message(msg)
+        message.send_message(msg, disable_web_page_preview=True)
 
     @staticmethod
     def delete_feed(message: BotCommand):
@@ -45,22 +48,23 @@ class CommandHandler:
             list_dicts_urls = message.list_feed()
             text = 'For unsubscribe send:\n'
             for item in range(0, len(list_dicts_urls)):
-                url = choice(list_dicts_urls)
+                url = random.choice(list_dicts_urls)
                 text += f"`/delete_feed {url['url']}`\n"
                 list_dicts_urls.remove(url)
-            message.send_message(text, parse_mode_markdown, True)
+            message.send_message(text, PARSE_MODE_MARKDOWN, True)
         else:
             try:
                 url = UrlFeed(url_feed=message.text)
             except ValidationError:
                 message.send_message('Bad url')
                 return
-            if message.delete_feed(str(url)):
+            if message.delete_feed(url.url_feed):
                 message.send_message(
-                    f'URL {str(url)} has been removed from your feed', disable_web_page_preview=True
+                    f'URL {url.url_feed} has been removed from your feed',
+                    disable_web_page_preview=True
                 )
-
-
+            else:
+                message.send_message('такого урла не найдено')
 
 
 # {
