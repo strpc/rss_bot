aa5dfd77773df00cebc3ef208f37337c8d0555e1
strpc <renat.kruchinskij@gmail.com>
2020-09-21 22:43:53 +0300

    добавил логику таски по рассылке сообщений.

--- src/core/queues/tasks.py
+++ src/core/queues/tasks.py
@@ -1,19 +1,22 @@
 from datetime import datetime
 
-# from src.core.queues.app import app
+from src.core.queues.app import app
 from src.core.requests.database import Database
 from src.core.schemas.rss import RssFeed
+from src.core.schemas.update import FromDB
 from src.core.utils import make_hash
+from src.project.settings import (COUNT_TEXT_SYMBOL, COUNT_TITLE_SYMBOL, PARSE_MODE_MARKDOWN,
+                                  )
 
 
-# @app.task()
+@app.task()
 def run_chain():
     chain = load_new_articles.s() | send_new_articles.s()
     chain()
 
 
-# @app.task()
-def load_new_articles():
+@app.task()
+def load_new_articles(*args, **kwargs):
     db = Database()
     active_feeds = db.get_active_feeds()
     if active_feeds is None:
@@ -34,13 +37,27 @@ def load_new_articles():
         db.insert_articles(values_for_execute)
 
 
-# @app.task()
-def send_new_articles():
-    from pprint import pprint
+@app.task()
+def send_new_articles(*args, **kwargs):
     db = Database()
     ready_articles = db.get_ready_articles()
-    pprint(ready_articles)
+    values_for_execute = []
+    if ready_articles is None:
+        return
+    for item in ready_articles:
+        text = f"*{item['title'][:COUNT_TITLE_SYMBOL]}*\n\n" if item['title'] else ''
+        text += f"{item['text'][:COUNT_TEXT_SYMBOL]}...\n\n" if item['title'] else ''
+        text += item['url_article']
 
+        message = FromDB(item['chat_id_id'])
+        response = message.send_message(text, PARSE_MODE_MARKDOWN, False)
 
-if __name__ == '__main__':
-    send_new_articles()
+        values_for_execute.append((item['url_article'], item['chat_id_id']))
+
+        if (response.status_code != 200 and
+                response.json().get('description') == "Forbidden: bot was blocked by the user"):
+            # пользователь остановил бота
+            db.disable_user(item['chat_id_id'])
+        elif response.status_code != 200:
+            message.send_message(text, disable_web_page_preview=False)
+    db.mark_sended(values_for_execute)
