6541c75e5dec24b480b394a282e9dd0d1ceb0cd1
strpc <renat.kruchinskij@gmail.com>
2021-02-15 02:00:49 +0300

    вынес всю логику в диспетчер

--- app/bot/views.py
+++ app/bot/views.py
@@ -1,15 +1,10 @@
 import json
 import logging
-from pprint import pprint
-from typing import Union, Optional
 
-from django.http import JsonResponse, HttpRequest
+from django.http import HttpRequest, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-from app.core.factory import Factory
-from app.core.handlers import CommandHandler
-from app.core.schemas.update import BaseMessage, BotCommand, TypeUpdate
-
+from app.core.handlers import dispatcher
 
 logger = logging.getLogger(__name__)
 
@@ -18,25 +13,6 @@ logger = logging.getLogger(__name__)
 def token_handler(request: HttpRequest) -> JsonResponse:
     if request.method == "POST":
         body = json.loads(request.body, encoding='utf-8')
-        # logger.info(body)
-        print('body:')
-        pprint(body)
-        message: Optional[Union[BaseMessage, BotCommand]] = Factory.register_message(body)
-        if not message:
-            # пришло какое-то событие, которое мы не отслеживаем.
-            return JsonResponse({'success': 'true'}, status=200)
-        print("\n\nfrom token_handler:")
-        pprint(message.__dict__)
-        if (message.type_update == TypeUpdate.command.value and
-                message.command_raw in CommandHandler.__dict__.keys()):
-            getattr(CommandHandler, message.command_raw)(message)
-
-        elif (
-                message.type_update == TypeUpdate.message.value
-                or message.type_update == TypeUpdate.command.value
-        ):
-            pass  # пришло обычное сообщение. нужно сделать заглушку.
-            print('обычное сообщение')
-            print(message.text)
-
+        logger.debug(body)
+        dispatcher.process(body)
         return JsonResponse({'success': 'true'}, status=200)
