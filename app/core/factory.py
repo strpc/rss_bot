e517308c5b424df98b6a1aadf272bfd82735be8d
strpc <renat.kruchinskij@gmail.com>
2021-02-15 01:55:06 +0300

    старт рефакторинга фабрики. возможно под нож пойдет

--- app/core/factory.py
+++ app/core/factory.py
@@ -1,32 +1,39 @@
+import logging
+import traceback
 from typing import Dict
 
-from app.core.schemas.update import Message, BotCommand
+from pydantic import ValidationError
+
+from app.core.updates import Message, Callback
+# from app.core.schemas.update import Message, BotCommand
+
+
+logger = logging.getLogger(__name__)
 
 
 class ObjectFactory:
     def __init__(self, list_obj: Dict):
         self._builders = list_obj
 
-    def register_message(self, message: Dict):
-        if 'message' not in message.keys():
-            return
-        type_msg = 'message'
-        entitles = message.get('message').get('entities')
-        if entitles:
-            for entitle in entitles:
-                for k, v in entitle.items():
-                    if v in self._builders.keys():
-                        type_msg = v
-
-        obj = self._builders.get(type_msg)
-        return obj(**message)
+    def register_message(self, update: Dict):
+        msg = None
+        try:
+            msg = Message.parse_obj(update)
+        except ValidationError:
+            msg = Callback.parse_obj(update)
+        except Exception as error:
+            logger.error('%s, %s\n\n%s', error, update, traceback.format_exc())
+        return msg
+        # obj = self._builders.get(type_msg)
+        # return obj(**update)
 
 
-type_obj = {
-    'bot_command': BotCommand,
-    'code': BotCommand,
-    'message': Message,
-}
+# type_obj = {
+#     'bot_command': BotCommand,
+#     'code': BotCommand,
+#     'message': Message,
+# }
 
 
-Factory = ObjectFactory(type_obj)
+# Factory = ObjectFactory(type_obj)
+Factory = ObjectFactory({})
