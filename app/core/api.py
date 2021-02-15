843de8ee3d417f271b0c9e7fe5ad5b199842b6ef
strpc <renat.kruchinskij@gmail.com>
2021-02-16 02:07:52 +0300

    сделал абстрактные классы. порефакторил

--- app/core/api.py
+++ app/core/api.py
@@ -1,32 +1,54 @@
+import logging
 from abc import ABC, abstractmethod
 from time import sleep
-from typing import Dict
-import logging
+from typing import Dict, Optional
 
 import httpx
 
-from app.project.settings import RSS_BOT_TOKEN, API_BASE_URL, ATTEMPT_REQUEST, DELAY_REQUEST
-
+from app.project.settings import (API_BASE_URL, ATTEMPT_REQUEST, DELAY_REQUEST,
+                                  RSS_BOT_TOKEN)
 
 logger = logging.getLogger(__name__)
 API_BASE_URL = API_BASE_URL[:-1] if API_BASE_URL.endswith('/') else API_BASE_URL
 REQUEST_URL = f"{API_BASE_URL}/bot{RSS_BOT_TOKEN}/"
 
 
-class Client(ABC):
-    """Обертка над запросами в API"""
+class Telegram(ABC):
+    @abstractmethod
+    def send_message(
+            self,
+            chat_id: int,
+            text: str,
+            parse_mode: Optional[str] = None,
+            disable_web_page_preview: bool = False
+    ): ...
+
+
+class ClientABC(ABC):
+    @abstractmethod
+    def get(self, method: str, params: Dict, attempt=ATTEMPT_REQUEST): ...
 
     @abstractmethod
-    def __init__(self):
-        pass
+    def post(
+            self,
+            method: str,
+            params=None,
+            body=None,
+            data=None,
+            attempt=ATTEMPT_REQUEST
+    ): ...
+
+
+class Client(ClientABC):
+    """Обертка над запросами в API"""
 
     @staticmethod
-    def __method_edit(method: str) -> str:
+    def _method_edit(method: str) -> str:
         return method[1:] if method.startswith('/') else method
 
     def get(self, method: str, params: Dict, attempt=ATTEMPT_REQUEST) -> httpx.Response:
         """Обертка над get-запросом"""
-        r = httpx.get(url=REQUEST_URL + self.__method_edit(method), params=params)
+        r = httpx.get(url=REQUEST_URL + self._method_edit(method), params=params)
 
         if r.status_code != 200 and attempt:
             sleep(DELAY_REQUEST)
@@ -50,7 +72,7 @@ class Client(ABC):
     ) -> httpx.Response:
         """Обертка над post-запросом"""
         r = httpx.post(
-            url=REQUEST_URL + self.__method_edit(method), json=body, params=params, data=data
+            url=REQUEST_URL + self._method_edit(method), json=body, params=params, data=data
         )
         if r.status_code == 200:
             return r
@@ -73,25 +95,26 @@ class Client(ABC):
             return r
 
 
-class Request(Client):
-    def __init__(self, chat_id: int):
-        super().__init__()
-        self.chat_id = chat_id
+class TelegramClient(Telegram):
+
+    def __init__(self, client: Client):
+        self._client = client
 
     def send_message(
             self,
+            chat_id: int,
             text: str,
-            parse_mode: str = None,
+            parse_mode: Optional[str] = None,
             disable_web_page_preview: bool = False
     ) -> httpx.Response:
         """Отправка сообщений пользователю"""
         method = "sendMessage"
         body = {
-            "chat_id": self.chat_id,
+            "chat_id": chat_id,
             "text": text
         }
         if parse_mode:
             body['parse_mode'] = parse_mode
         if disable_web_page_preview:
             body['disable_web_page_preview'] = True
-        return self.post(method=method, body=body)
+        return self._client.post(method=method, body=body)
