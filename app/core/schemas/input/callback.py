45e69a2e61bf0c958737ec71be1a40efcdd213ac
strpc <renat.kruchinskij@gmail.com>
2021-02-16 02:08:12 +0300

    отсортировал импорты

--- app/core/schemas/input/callback.py
+++ app/core/schemas/input/callback.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 from pydantic import BaseModel, Field
 
-from app.core.schemas.input.base import User, TypeUpdate
+from app.core.schemas.input.base import TypeUpdate, User
 from app.core.schemas.input.message import MessageBody
 
 
