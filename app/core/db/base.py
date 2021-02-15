6296e551c6762514c20ac0371259a8f800bd485f
strpc <renat.kruchinskij@gmail.com>
2021-02-16 02:05:54 +0300

    вынес в абстрактный модуль все методы для работы с базой

---
+++ app/core/db/base.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+
+class Database(ABC):
+    @abstractmethod
+    def register_user(self, obj: Tuple[Any]): ...
+
+    @abstractmethod
+    def init_user(self, chat_id: int) -> bool: ...
+
+    @abstractmethod
+    def disable_user(self, chat_id: int): ...
+
+    @abstractmethod
+    def add_feed(self, values: List[Tuple]): ...
+
+    @abstractmethod
+    def delete_feed(self, url: str, chat_id: int) -> bool: ...
+
+    @abstractmethod
+    def list_feed(self, chat_id: int) -> Optional[List[Dict]]: ...
+
+    @abstractmethod
+    def find_active_url(self, url: str, chat_id: int) -> Optional[List[Dict]]: ...
+
+    @abstractmethod
+    def get_active_feeds(self) -> Optional[List[Dict]]: ...
+
+    @abstractmethod
+    def insert_articles(self, values: Union[List, Tuple]): ...
+
+    @abstractmethod
+    def get_ready_articles(self) -> Optional[List[Dict]]: ...
+
+    @abstractmethod
+    def mark_sended(self, values: Union[List, Tuple]): ...
