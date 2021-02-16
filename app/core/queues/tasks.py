9b3fb9380f007cd8970125b88dba5beb5514babd
strpc <renat.kruchinskij@gmail.com>
2021-02-17 02:19:19 +0300

    переименовал функцию получения хеша

--- app/core/queues/tasks.py
+++ app/core/queues/tasks.py
@@ -4,9 +4,8 @@ from app.core.queues.app import app
 from app.core.requests.database import Database
 from app.core.schemas.rss import RssFeed
 from app.core.schemas.update import FromDB
-from app.core.utils import make_hash
-from app.project.settings import (COUNT_TEXT_SYMBOL, COUNT_TITLE_SYMBOL, PARSE_MODE_MARKDOWN,
-                                  )
+from app.core.utils import get_hash
+from app.project.settings import COUNT_TEXT_SYMBOL, COUNT_TITLE_SYMBOL, PARSE_MODE_MARKDOWN
 
 
 @app.task()
@@ -23,17 +22,25 @@ def load_new_articles(*args, **kwargs):
         return
 
     for item in active_feeds:
-        url_rss = item['url']
-        chat_id = item['chat_id_id']
-        rss_hash = item['chatid_url_hash']
+        url_rss = item["url"]
+        chat_id = item["chat_id_id"]
+        rss_hash = item["chatid_url_hash"]
         feed = RssFeed(url=url_rss)
         articles = feed.parse()
 
         values_for_execute = []
         for article in articles:
             values_for_execute.append(
-                (article.url, article.title, article.text, datetime.utcnow(),
-                 False, make_hash(article.url, chat_id), rss_hash, chat_id)
+                (
+                    article.url,
+                    article.title,
+                    article.text,
+                    datetime.utcnow(),
+                    False,
+                    get_hash(article.url, chat_id),
+                    rss_hash,
+                    chat_id,
+                )
             )
         db.insert_articles(values_for_execute)
 
@@ -46,21 +53,21 @@ def send_new_articles(*args, **kwargs):
     if ready_articles is None:
         return
     for item in ready_articles:
-        text = f"*{item['title'][:COUNT_TITLE_SYMBOL]}*\n\n" if item['title'] else ''
-        text += f"{item['text'][:COUNT_TEXT_SYMBOL]}...\n\n" if item['text'] else ''
-        text += item['url_article']
+        text = f"*{item['title'][:COUNT_TITLE_SYMBOL]}*\n\n" if item["title"] else ""
+        text += f"{item['text'][:COUNT_TEXT_SYMBOL]}...\n\n" if item["text"] else ""
+        text += item["url_article"]
 
-        message = FromDB(item['chat_id_id'])
+        message = FromDB(item["chat_id_id"])
         response = message.send_message(text, PARSE_MODE_MARKDOWN, False)
 
-        values_for_execute.append((item['url_article'], item['chat_id_id']))
+        values_for_execute.append((item["url_article"], item["chat_id_id"]))
 
         if (
-                response.status_code != 200
-                and response.json().get('description') == "Forbidden: bot was blocked by the user"
+            response.status_code != 200
+            and response.json().get("description") == "Forbidden: bot was blocked by the user"
         ):
             # пользователь остановил бота
-            db.disable_user(item['chat_id_id'])
+            db.disable_user(item["chat_id_id"])
         elif response.status_code != 200:
-            message.send_message(text.replace('*', ''), disable_web_page_preview=False)
+            message.send_message(text.replace("*", ""), disable_web_page_preview=False)
     db.mark_sended(values_for_execute)
