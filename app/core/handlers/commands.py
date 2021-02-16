b1fab837789fc99ef2d014506ba366376df3bf72
strpc <renat.kruchinskij@gmail.com>
2021-02-17 02:11:01 +0300

    отрефакторил start, начал рефакторить add_feed

--- app/core/handlers/commands.py
+++ app/core/handlers/commands.py
@@ -1,14 +1,13 @@
 import logging
 import random
-from datetime import datetime
 
 from pydantic import ValidationError
 
+from app.core import schemas
 from app.core.clients import ITelegram
 from app.core.db import IDatabase
 from app.core.schemas.rss import ListUrls, UrlFeed
-from app.core.schemas.update import BotCommand
-from app.core.utils import make_hash, make_str_urls
+from app.core.utils import get_hash, make_str_urls
 from app.project.settings import PARSE_MODE_MARKDOWN
 
 
@@ -20,51 +19,61 @@ class CommandHandler:
         self._db = database
         self._telegram = telegram
 
-    @staticmethod
-    def start(message: BotCommand):
-        message.init_user()
-        if message.new_user:
-            message.register_user()
-            message.request.send_message('здарова дятел')
-            # первый старт/рестарт пользователем
-        # повторный /start без остановки бота
-        message.send_message('hello world')
-        print('hello method start')
+    def start(self, update: schemas.Message):
+        logger.debug("/start enter...")
+        if self._db.registered_user(update.message.chat.id) is True:
+            logger.warning("Авторизированный юзер: %s", update)
+            return
 
-    @staticmethod
-    def add_feed(message: BotCommand):
-        list_urls = ListUrls.get_instance(message.text)
+        self._db.register_user(
+            chat_id=update.message.chat.id,
+            first_name=update.message.user.first_name,
+            last_name=update.message.user.last_name,
+            username=update.message.user.username,
+        )
+        logger.debug(
+            "Пользователь username=%s, chat_id=%s зарегестрирован",
+            update.message.user.username,
+            update.message.chat.id,
+        )
+        message = self._db.get_service_message("hello_user")
+        self._telegram.send_message(chat_id=update.message.chat.id, text=message["message"])
 
-        values_for_execute = []
+    def add_feed(self, update: schemas.Message):
+        # todo: добавить ограничение на кол-во записей зараз. из конфига
+        logger.debug("/add_feed enter...")
+        list_urls = ListUrls.get_instance(update.message.text)
+        chat_id = update.message.chat.id
+
         for url in list_urls:
-            hash_url_chat_id = make_hash(url, message.chat_id)
-            values_for_execute.append(
-                (str(url), datetime.utcnow(), True, message.chat_id, hash_url_chat_id)
-            )
-        message.add_feed(values_for_execute)
+            logger.debug("Process url=%s...", url)
+            hash_url = get_hash(url, update.message.chat.id)
+            self._db.add_feed(url=url, chat_id=chat_id, hash_url=hash_url)
+            logger.info("Добавлен новый фид url=%s chat_id=%s", url, chat_id)
 
-        text = 'твои ссылки - шляпа'  # заглушка, если вдруг пришла одна ссылка и она не rss
         if len(list_urls) > 1:
             text = f"URLs: {make_str_urls(list_urls)} \nhas beed added to your feed."
         elif len(list_urls) == 1:
             text = f"URL: {make_str_urls(list_urls)} \nhas beed added to your feed."
-        message.send_message(text, disable_web_page_preview=True)
+        else:
+            text = "твои ссылки - шляпа"  # заглушка, если вдруг пришла одна ссылка и она не rss
+        self._telegram.send_message(chat_id=chat_id, text=text, disable_web_page_preview=True)
 
     @staticmethod
-    def list_feed(message: BotCommand):
+    def list_feed(message):
         list_dicts_urls = message.list_feed(message.chat_id)
         if list_dicts_urls is None:
             msg = "You not have active subscriptions."
         else:
-            list_str_urls = [i.get('url') for i in list_dicts_urls]
-            msg = f'You are subscribed to:\n{make_str_urls(list_str_urls)}'
+            list_str_urls = [i.get("url") for i in list_dicts_urls]
+            msg = f"You are subscribed to:\n{make_str_urls(list_str_urls)}"
         message.send_message(msg, disable_web_page_preview=True)
 
     @staticmethod
-    def delete_feed(message: BotCommand):
+    def delete_feed(message):
         if not message.text:
             list_dicts_urls = message.list_feed(message.chat_id)
-            text = 'For unsubscribe send:\n'
+            text = "For unsubscribe send:\n"
             for item in range(0, len(list_dicts_urls)):
                 url = random.choice(list_dicts_urls)
                 text += f"`/delete_feed {url['url']}`\n"
@@ -74,12 +83,11 @@ class CommandHandler:
             try:
                 url = UrlFeed(url_feed=message.text)
             except ValidationError:
-                message.send_message('Bad url')
+                message.send_message("Bad url")
                 return
             if message.delete_feed(url.url_feed, message.chat_id):
                 message.send_message(
-                    f'URL {url.url_feed} has been removed from your feed',
-                    disable_web_page_preview=True
+                    f"URL {url.url_feed} has been removed from your feed", disable_web_page_preview=True
                 )
             else:
-                message.send_message('такого урла не найдено')
+                message.send_message("такого урла не найдено")
