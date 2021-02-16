31a9bfe8a28d76150ddeeb62c7d4afb7b2144a5c
strpc <renat.kruchinskij@gmail.com>
2021-02-16 21:54:25 +0300

    переименовал api -> telegram

---
+++ app/core/clients/telegram.py
@@ -0,0 +1,45 @@
+import logging
+from abc import ABC, abstractmethod
+from typing import Optional
+
+import httpx
+
+from app.core.clients.requests_ import IClient
+
+logger = logging.getLogger(__name__)
+
+
+class Telegram(ABC):
+    @abstractmethod
+    def send_message(
+            self,
+            chat_id: int,
+            text: str,
+            parse_mode: Optional[str] = None,
+            disable_web_page_preview: bool = False
+    ): ...
+
+
+class TelegramClient(Telegram):
+
+    def __init__(self, client: IClient):
+        self._client = client
+
+    def send_message(
+            self,
+            chat_id: int,
+            text: str,
+            parse_mode: Optional[str] = None,
+            disable_web_page_preview: bool = False
+    ) -> httpx.Response:
+        """Отправка сообщений пользователю"""
+        method = "sendMessage"
+        body = {
+            "chat_id": chat_id,
+            "text": text
+        }
+        if parse_mode:
+            body['parse_mode'] = parse_mode
+        if disable_web_page_preview:
+            body['disable_web_page_preview'] = True
+        return self._client.post(method=method, body=body)
--- app/core/handlers/commands.py
+++ app/core/handlers/commands.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 from pydantic import ValidationError
 
-from app.core.clients.api import Telegram
+from app.core.clients.telegram import Telegram
 from app.core.db import Database
 from app.core.schemas.rss import ListUrls, UrlFeed
 from app.core.schemas.update import BotCommand
