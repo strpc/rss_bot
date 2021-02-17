053b2fbe0dc7614bc4a99e15c4cecf07743a8207
strpc <renat.kruchinskij@gmail.com>
2021-02-18 02:09:33 +0300

    добавил новую таблицу в админку. отформатировал по black

--- app/bot/admin.py
+++ app/bot/admin.py
@@ -1,6 +1,7 @@
+from django import forms
 from django.contrib import admin
 
-from .models import Article, RSS, User
+from .models import RSS, Article, ServiceMessage, User
 
 
 @admin.register(User)
@@ -9,31 +10,42 @@ class UserAdmin(admin.ModelAdmin):
     save_as = True
     empty_value_display = "Недоступно"
     fields = (
-        'chat_id', 'first_name', 'last_name', 'username',
-        'register', 'active', 'view_rss_feeds',
+        "chat_id",
+        "first_name",
+        "last_name",
+        "username",
+        "register",
+        "active",
+        "view_rss_feeds",
     )
     readonly_fields = (
-        'chat_id', 'first_name', 'last_name', 'username',
-        'register', 'view_rss_feeds',
+        "chat_id",
+        "first_name",
+        "last_name",
+        "username",
+        "register",
+        "view_rss_feeds",
     )
     list_display = (
-        'username', 'register', 'active',
+        "username",
+        "register",
+        "active",
     )
     list_display_links = (
-        'username', 'register', 'active',
+        "username",
+        "register",
+        "active",
     )
-    list_filter = (
-        'active',
-    )
+    list_filter = ("active",)
 
     def view_rss_feeds(self, obj):
         rss_query = RSS.objects.filter(chat_id=obj.chat_id, active=True)
-        result = ''
+        result = ""
         for item in rss_query:
             result += f"{item.url}\n"
         return result
 
-    view_rss_feeds.short_description = 'Active RSS'
+    view_rss_feeds.short_description = "Active RSS"
 
 
 @admin.register(RSS)
@@ -42,27 +54,30 @@ class AdminRss(admin.ModelAdmin):
     save_as = True
     empty_value_display = "Недоступно"
     fields = (
-        'view_username', 'url', 'added', 'active',
+        "view_username",
+        "url",
+        "added",
+        "active",
     )
-    readonly_fields = (
-        'url', 'added', 'view_username'
-    )
-    exclude = (
-        'chatid_url_hash',
-    )
+    readonly_fields = ("url", "added", "view_username")
+    exclude = ("chatid_url_hash",)
     list_display = (
-        'view_username', 'url', 'added', 'active',
+        "view_username",
+        "url",
+        "added",
+        "active",
     )
-    list_display_links = (
-        'url', 'added', 'active', 'view_username'
-    )
+    list_display_links = ("url", "added", "active", "view_username")
     list_filter = (
-        'chat_id__username', 'active', 'url',
+        "chat_id__username",
+        "active",
+        "url",
     )
 
     def view_username(self, obj):
         return view_username(obj)
-    view_username.short_description = 'Username'
+
+    view_username.short_description = "Username"
 
 
 @admin.register(Article)
@@ -70,31 +85,65 @@ class AdminArticle(admin.ModelAdmin):
     save_on_top = True
     save_as = True
     empty_value_display = "Недоступно"
-    ordering = ('added', 'chat_id__pk')
+    ordering = ("added", "chat_id__pk")
     fields = (
-        'view_username', 'title', 'get_rss_url', 'added', 'sended', 'text',
+        "view_username",
+        "title",
+        "get_rss_url",
+        "added",
+        "sended",
+        "text",
     )
     readonly_fields = (
-        'view_username', 'title', 'get_rss_url', 'added', 'sended', 'text',
+        "view_username",
+        "title",
+        "get_rss_url",
+        "added",
+        "sended",
+        "text",
     )
-    list_display = (
-        'view_username', 'title', 'get_rss_url', 'added', 'sended'
-    )
+    list_display = ("view_username", "title", "get_rss_url", "added", "sended")
     list_display_links = (
-        'view_username', 'title', 'get_rss_url', 'added',
+        "view_username",
+        "title",
+        "get_rss_url",
+        "added",
     )
     list_filter = (
-        'sended', 'chat_id__username', 'rss_url__url',
+        "sended",
+        "chat_id__username",
+        "rss_url__url",
     )
 
     def view_username(self, obj):
         return view_username(obj)
-    view_username.short_description = 'Username'
+
+    view_username.short_description = "Username"
 
 
 def view_username(obj):
     return f"@{obj.chat_id.username}"
 
 
-admin.site.site_title = 'RSS bot'
-admin.site.site_header = 'RSS bot'
+class ServiceMessageForm(forms.ModelForm):
+    class Meta:
+        model = ServiceMessage
+        fields = "__all__"
+        widgets = {
+            "text": forms.Textarea(attrs={"cols": 80, "rows": 20}),
+        }
+
+
+@admin.register(ServiceMessage)
+class AdminServiceMessage(admin.ModelAdmin):
+    save_on_top = True
+    save_as = True
+    fields = ("title", "text")
+    ordering = ("title", "text")
+    list_display = ("title", "text")
+    list_display_links = ("title", "text")
+    form = ServiceMessageForm
+
+
+admin.site.site_title = "RSS bot"
+admin.site.site_header = "RSS bot"
