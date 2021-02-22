067ad82005c9fdcd6c059358026277effd3bc3ec
strpc <renat.kruchinskij@gmail.com>
2021-02-23 01:08:21 +0300

    отрефакторил оставиеся команды. изменил нейминг классов

--- app/core/handlers/commands.py
+++ app/core/handlers/commands.py
@@ -6,9 +6,9 @@ from pydantic import ValidationError
 from app.core import schemas
 from app.core.clients import ITelegram
 from app.core.db import IDatabase
-from app.core.schemas.rss import ListUrls, UrlFeed
+from app.core.schemas.rss import Feed, ListFeeds
 from app.core.utils import get_hash, make_str_urls
-from app.project.settings import PARSE_MODE_MARKDOWN
+from app.project.settings import COUNT_FEED_FOR_ADD, PARSE_MODE_MARKDOWN
 
 
 logger = logging.getLogger(__name__)
@@ -36,19 +36,19 @@ class CommandHandler:
             update.message.user.username,
             update.message.chat.id,
         )
-        message = self._db.get_service_message("hello_user")
-        self._telegram.send_message(chat_id=update.message.chat.id, text=message["text"])
+        message = self._db.get_service_message("hello_user")["text"]
+        self._telegram.send_message(chat_id=update.message.chat.id, text=message)
 
     def add_feed(self, update: schemas.Message):
         # todo: добавить ограничение на кол-во записей зараз. из конфига
         logger.debug("/add_feed enter...")
-        list_urls = ListUrls.get_instance(update.message.text)
+        list_urls = ListFeeds(feeds=update.message.text)
         chat_id = update.message.chat.id
 
-        for url in list_urls:
+        for url in list_urls[:COUNT_FEED_FOR_ADD]:
             logger.debug("Process url=%s...", url)
             hash_url = get_hash(url, update.message.chat.id)
-            self._db.add_feed(url=url, chat_id=chat_id, hash_url=hash_url)
+            self._db.add_feed(url=str(url), chat_id=chat_id, hash_url=hash_url)
             logger.info("Добавлен новый фид url=%s chat_id=%s", url, chat_id)
 
         if len(list_urls) > 1:
@@ -56,39 +56,55 @@ class CommandHandler:
         elif len(list_urls) == 1:
             text = f"URL: {make_str_urls(list_urls)} \nhas beed added to your feed."
         else:
-            text = "твои ссылки - шляпа"  # заглушка, если вдруг пришла одна ссылка и она не rss
+            text = self._db.get_service_message("incorrect_rss")["text"]
+
         self._telegram.send_message(chat_id=chat_id, text=text, disable_web_page_preview=True)
 
-    @staticmethod
-    def list_feed(message):
-        list_dicts_urls = message.list_feed(message.chat_id)
-        if list_dicts_urls is None:
-            msg = "You not have active subscriptions."
-        else:
-            list_str_urls = [i.get("url") for i in list_dicts_urls]
-            msg = f"You are subscribed to:\n{make_str_urls(list_str_urls)}"
-        message.send_message(msg, disable_web_page_preview=True)
+    def list_feed(self, update: schemas.Message):
+        chat_id = update.message.chat.id
 
-    @staticmethod
-    def delete_feed(message):
-        if not message.text:
-            list_dicts_urls = message.list_feed(message.chat_id)
+        list_dicts_urls = self._db.list_feed(chat_id)
+
+        if list_dicts_urls is None:
+            msg = self._db.get_service_message("not_have_active")["text"]
+        else:
+            list_str_urls = [url.get("url") for url in list_dicts_urls]
+            msg = f"You are subscribed to:\n{make_str_urls(list_str_urls)}"
+
+        self._telegram.send_message(chat_id=chat_id, text=msg, disable_web_page_preview=True)
+
+    def delete_feed(self, update: schemas.Message):
+        chat_id = update.message.chat.id
+        user_input = update.message.text.replace("/delete_feed", "")
+
+        if not user_input:
+            list_dicts_urls = self._db.list_feed(chat_id)
+
             text = "For unsubscribe send:\n"
-            for item in range(0, len(list_dicts_urls)):
-                url = random.choice(list_dicts_urls)
+            while list_dicts_urls:
+                url = list_dicts_urls.pop(random.randint(0, len(list_dicts_urls) - 1))
                 text += f"`/delete_feed {url['url']}`\n"
-                list_dicts_urls.remove(url)
-            message.send_message(text, PARSE_MODE_MARKDOWN, True)
+
+            self._telegram.send_message(
+                chat_id=chat_id,
+                text=text,
+                parse_mode=PARSE_MODE_MARKDOWN,
+                disable_web_page_preview=True,
+            )
         else:
             try:
-                url = UrlFeed(url_feed=message.text)
+                url = Feed(url=user_input)
             except ValidationError:
-                message.send_message("Bad url")
+                msg = self._db.get_service_message("incorrect_rss")["text"]
+                self._telegram.send_message(chat_id=chat_id, text=msg)
                 return
-            if message.delete_feed(url.url_feed, message.chat_id):
-                message.send_message(
-                    f"URL {url.url_feed} has been removed from your feed",
+
+            if self._db.delete_feed(url.url, chat_id):
+                self._telegram.send_message(
+                    chat_id=chat_id,
+                    text=f"URL {url.url} has been removed from your feed",
                     disable_web_page_preview=True,
                 )
             else:
-                message.send_message("такого урла не найдено")
+                msg = self._db.get_service_message("url_not_founded")["text"]
+                self._telegram.send_message(chat_id=chat_id, text=msg)
