22ce271b812824bf720328258cd1da39b09ac332
strpc <renat.kruchinskij@gmail.com>
2021-05-26 02:21:55 +0300

    добавил схему сообщения

---
+++ backend/app/schemas/message.py
@@ -0,0 +1,58 @@
+import re
+from datetime import datetime
+from typing import Dict, List, Optional
+
+from app.schemas.enums import TypeChat, TypeEntity, TypeUpdate
+from pydantic import BaseModel, Field, validator
+
+
+class User(BaseModel):
+    id: int
+    is_bot: bool
+    first_name: str
+    last_name: Optional[str] = None
+    username: Optional[str] = None
+    language_code: Optional[str] = None
+
+
+class Chat(BaseModel):
+    id: int
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    type: TypeChat
+    username: Optional[str] = None
+
+
+class MessageEntity(BaseModel):
+    length: int
+    offset: int
+    type: TypeEntity
+    url: Optional[str] = None
+    user: Optional[str] = None
+    language: Optional[str] = None
+
+
+class MessageBody(BaseModel):
+    chat: Chat
+    date: datetime
+    entities: Optional[List[MessageEntity]] = None
+    user: User = Field(..., alias="from")
+    message_id: int
+    text: str
+    command: Optional[str] = None
+
+    @validator("command", always=True)
+    def validate_command(cls, v, values: Dict) -> Optional[str]:
+        text = values.get("text")
+        if text is None:
+            return v
+
+        command_compile = re.compile(r"(^|\s)\/\b[^_][a-zA-Z_]+\b")
+        command = re.search(command_compile, text)
+        return command[0][1:] if command else None
+
+
+class Message(BaseModel):
+    message: MessageBody
+    update_id: int
+    type_update: TypeUpdate = TypeUpdate.message
