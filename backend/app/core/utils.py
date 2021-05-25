d3a6992a7f8a41d6d8459519af3e5a4261360ed6
strpc <renat.kruchinskij@gmail.com>
2021-05-26 02:20:01 +0300

    добавил утилиты

--- backend/app/core/utils.py
+++ backend/app/core/utils.py
@@ -6,10 +6,19 @@ from urllib.parse import urlparse
 import feedparser
 
 
-def from_timestamp(unixtime: Optional[int] = None) -> Optional[datetime]:
-    if unixtime is None:
-        return None
-    return datetime.fromtimestamp(unixtime)
+def shielding_markdown_text(text: str) -> str:
+    new_text = (
+        text.replace("_", "\\_")
+        .replace("*", "\\*")
+        .replace("[", "\\[")
+        .replace("`", "\\`")
+        .replace(".", "\\.")
+    )
+    return new_text
+
+
+def bold_markdown(text: str) -> str:
+    return f"*{text}*"
 
 
 def get_hash(*args) -> str:
@@ -21,14 +30,23 @@ def get_hash(*args) -> str:
     return base64.b64encode(hash_.encode()).decode()
 
 
+def validate_url(url: str) -> bool:
+    parsed = urlparse(url)
+    return all([parsed.scheme, parsed.netloc])
+
+
+# todo: ну вот хз нужно ли то, что ниже
+
+
+def from_timestamp(unixtime: Optional[int] = None) -> Optional[datetime]:
+    if unixtime is None:
+        return None
+    return datetime.fromtimestamp(unixtime)
+
+
 def make_str_urls(urls: Iterable[str]) -> str:
     return "\n".join(urls)
 
 
 def validate_feed(url: str) -> bool:
     return bool(feedparser.parse(url).get("entries"))
-
-
-def validate_url(url: str) -> bool:
-    parsed = urlparse(url)
-    return all([parsed.scheme, parsed.netloc])
