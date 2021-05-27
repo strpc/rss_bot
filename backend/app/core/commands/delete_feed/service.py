f8f222ca0a3b2af3b8a1820558f30084fdb061ae
strpc <renat.kruchinskij@gmail.com>
2021-05-27 22:39:14 +0300

    добавил прокидывание сервиса сервисных сообщений

--- backend/app/core/commands/delete_feed/service.py
+++ backend/app/core/commands/delete_feed/service.py
@@ -1,12 +1,20 @@
 from app.core.clients.telegram import Telegram
 from app.core.commands.delete_feed.repository import CommandDeleteFeedRepository
+from app.core.service_messages.service import ServiceMessagesService
 from app.schemas.message import Message
 
 
 class CommandDeleteFeedService:
-    def __init__(self, repository: CommandDeleteFeedRepository, telegram: Telegram):
-        self._telegram = telegram
+    def __init__(
+        self,
+        *,
+        repository: CommandDeleteFeedRepository,
+        telegram: Telegram,
+        service_messages: ServiceMessagesService,
+    ):
         self._repository = repository
+        self._telegram = telegram
+        self._service_messages = service_messages
 
     async def handle(self, update: Message) -> None:
         pass
--- backend/app/core/commands/list_feed/service.py
+++ backend/app/core/commands/list_feed/service.py
@@ -1,12 +1,20 @@
 from app.core.clients.telegram import Telegram
 from app.core.commands.list_feed.repository import CommandListFeedRepository
+from app.core.service_messages.service import ServiceMessagesService
 from app.schemas.message import Message
 
 
 class CommandListFeedService:
-    def __init__(self, repository: CommandListFeedRepository, telegram: Telegram):
+    def __init__(
+        self,
+        *,
+        repository: CommandListFeedRepository,
+        telegram: Telegram,
+        service_messages: ServiceMessagesService,
+    ):
         self._telegram = telegram
         self._repository = repository
+        self._service_messages = service_messages
 
     async def handle(self, update: Message) -> None:
         pass
