16c19d70aba2a6c70215afc375af43a4c2f7c0d9
strpc <renat.kruchinskij@gmail.com>
2021-05-27 03:07:44 +0300

    добавил трейсинг логов

--- backend/app/logger.py
+++ backend/app/logger.py
@@ -1,7 +1,30 @@
+import sys
+
 from loguru import logger
 
 from app.config import LogLevelEnum
 
 
 def configure_logging(log_level: LogLevelEnum) -> None:
-    logger.add("docker/logs/celery_log.log", rotation="10 MB", level=log_level)
+    logger.remove()
+    if not log_level.DEBUG:
+        logger.add(
+            "docker/logs/celery_log.log",
+            rotation="10 MB",
+            level=log_level,
+            format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+            "<level>{level: <8}</level> | "
+            "<cyan>{name}</cyan>:<cyan>{function}</cyan>:"
+            "<cyan>{line}</cyan> | chat_id={extra[chat_id]} | username={extra[username]} | "
+            "uuid={extra[uuid]} | - <level>{message}</level>",
+        )
+    logger.add(
+        sys.stdout,
+        level=log_level,
+        format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+        "<level>{level: <8}</level> | "
+        "<cyan>{name}</cyan>:<cyan>{function}</cyan>:"
+        "<cyan>{line}</cyan> | chat_id={extra[chat_id]} | username={extra[username]} | "
+        "uuid={extra[uuid]} | - <level>{message}</level>",
+    )
+    logger.configure(extra={"chat_id": 0, "username": '""', "uuid": '""'})
