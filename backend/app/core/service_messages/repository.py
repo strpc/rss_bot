ae3561df8d2f82323f1adb68d834be16aa9c373f
strpc <renat.kruchinskij@gmail.com>
2021-05-28 03:47:40 +0300

    добавил метод получение текста сообщения из базы

--- backend/app/core/service_messages/repository.py
+++ backend/app/core/service_messages/repository.py
@@ -1,6 +1,19 @@
+from typing import Optional
+
 from app.core.clients.database import Database
+from app.core.service_messages.models import ServiceMessage
 
 
 class ServiceMessagesRepository:
     def __init__(self, database: Database):
         self._db = database
+
+    async def get_message(self, title: ServiceMessage) -> Optional[str]:
+        query = """
+        SELECT text
+        FROM bot_service_message
+        WHERE title = ?
+        """
+        row = await self._db.fetchone(query, (title,))
+        if row is not None:
+            return row[0]  # type: ignore
