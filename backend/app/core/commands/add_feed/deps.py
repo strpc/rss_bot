1a7245b4ec689d6ab694c81c0edbfc8e504103ae
strpc <renat.kruchinskij@gmail.com>
2021-05-29 01:12:55 +0300

    добавил прокидывание сервиса фидов в сервисы команд

--- backend/app/core/commands/add_feed/deps.py
+++ backend/app/core/commands/add_feed/deps.py
@@ -1,27 +1,21 @@
 from fastapi import Depends
 
-from app.core.base_deps import get_database, get_telegram_client
-from app.core.clients.database import Database
+from app.core.base_deps import get_telegram_client
 from app.core.clients.telegram import Telegram
-from app.core.commands.add_feed.repository import CommandAddFeedRepository
 from app.core.commands.add_feed.service import CommandAddFeedService
+from app.core.feeds.deps import get_feeds_service
+from app.core.feeds.service import FeedsService
 from app.core.service_messages.deps import get_service_messages_service
 from app.core.service_messages.service import ServiceMessagesService
 
 
-def get_command_add_feed_repository(
-    db: Database = Depends(get_database),
-) -> CommandAddFeedRepository:
-    return CommandAddFeedRepository(database=db)
-
-
 def get_command_add_feed_service(
-    repository: CommandAddFeedRepository = Depends(get_command_add_feed_repository),
+    feeds_service: FeedsService = Depends(get_feeds_service),
     telegram: Telegram = Depends(get_telegram_client),
     service_messages: ServiceMessagesService = Depends(get_service_messages_service),
 ) -> CommandAddFeedService:
     return CommandAddFeedService(
-        repository=repository,
+        feeds_service=feeds_service,
         telegram=telegram,
         service_messages=service_messages,
     )
--- backend/app/core/commands/list_feed/deps.py
+++ backend/app/core/commands/list_feed/deps.py
@@ -1,27 +1,21 @@
 from fastapi import Depends
 
-from app.core.base_deps import get_database, get_telegram_client
-from app.core.clients.database import Database
+from app.core.base_deps import get_telegram_client
 from app.core.clients.telegram import Telegram
-from app.core.commands.list_feed.repository import CommandListFeedRepository
 from app.core.commands.list_feed.service import CommandListFeedService
+from app.core.feeds.deps import get_feeds_service
+from app.core.feeds.service import FeedsService
 from app.core.service_messages.deps import get_service_messages_service
 from app.core.service_messages.service import ServiceMessagesService
 
 
-def get_command_list_feed_repository(
-    db: Database = Depends(get_database),
-) -> CommandListFeedRepository:
-    return CommandListFeedRepository(database=db)
-
-
 def get_command_list_feed_service(
-    repository: CommandListFeedRepository = Depends(get_command_list_feed_repository),
+    feeds_service: FeedsService = Depends(get_feeds_service),
     telegram: Telegram = Depends(get_telegram_client),
     service_messages: ServiceMessagesService = Depends(get_service_messages_service),
 ) -> CommandListFeedService:
     return CommandListFeedService(
-        repository=repository,
+        feeds_service=feeds_service,
         telegram=telegram,
         service_messages=service_messages,
     )
