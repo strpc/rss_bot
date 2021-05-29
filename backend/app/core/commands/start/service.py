015e55c1bf2c879288982b7a12ae4cc3e70d67fb
strpc <renat.kruchinskij@gmail.com>
2021-05-29 03:55:23 +0300

    убрал репозиторий. ибо не нужен он здесь

--- backend/app/core/commands/start/service.py
+++ backend/app/core/commands/start/service.py
@@ -1,7 +1,6 @@
 from loguru import logger
 
 from app.core.clients.telegram import Telegram
-from app.core.commands.start.repository import CommandStartRepository
 from app.core.service_messages.models import ServiceMessage
 from app.core.service_messages.service import ServiceMessagesService
 from app.schemas.message import Message
@@ -11,12 +10,10 @@ class CommandStartService:
     def __init__(
         self,
         *,
-        repository: CommandStartRepository,
         telegram: Telegram,
         service_messages: ServiceMessagesService,
     ):
         self._telegram = telegram
-        self._repository = repository
         self._service_messages = service_messages
 
     async def handle(self, update: Message) -> None:
