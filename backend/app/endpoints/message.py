a59e0c893b18db3ae18625f7884ba63e6a574a70
strpc <renat.kruchinskij@gmail.com>
2021-05-29 23:41:30 +0300

    поправил импорты, аннотацию типов

--- backend/app/endpoints/message.py
+++ backend/app/endpoints/message.py
@@ -1,12 +1,7 @@
-from typing import Union
-
 from fastapi import APIRouter, Body, Depends, Response
 from loguru import logger
 
-from app.core.commands.add_feed.service import CommandAddFeedService
-from app.core.commands.delete_feed.service import CommandDeleteFeedService
-from app.core.commands.list_feed.service import CommandListFeedService
-from app.core.commands.start.service import CommandStartService
+from app.core.commands.command_abc import CommandServiceABC
 from app.core.service_messages.deps import get_service_messages_service
 from app.core.service_messages.models import ServiceMessage
 from app.core.service_messages.service import ServiceMessagesService
@@ -17,18 +12,12 @@ from app.schemas.message import Message
 
 
 router = APIRouter(route_class=LoggingRoute)
-CommandsServicesType = Union[
-    CommandStartService,
-    CommandAddFeedService,
-    CommandListFeedService,
-    CommandDeleteFeedService,
-]
 
 
 @router.post("/")
 async def new_message(
     update: Message = Body(...),
-    command_service: CommandsServicesType = Depends(get_command_service),
+    command_service: CommandServiceABC = Depends(get_command_service),
     service_messages: ServiceMessagesService = Depends(get_service_messages_service),
     current_user: User = Depends(get_current_user),
 ) -> Response:
