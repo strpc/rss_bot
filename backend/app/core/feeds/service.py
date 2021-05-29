edc537320c475310185c9151b4a9da9dfaeab33f
strpc <renat.kruchinskij@gmail.com>
2021-05-29 03:57:44 +0300

    добавил методы для проверки существования фида+юзера в базе, валидации фида и добавления фида в базу

--- backend/app/core/feeds/service.py
+++ backend/app/core/feeds/service.py
@@ -1,6 +1,25 @@
+from urllib.error import URLError
+
+import feedparser
+
 from app.core.feeds.repository import FeedsRepository
+from app.core.utils import get_hash, run_in_threadpool
 
 
 class FeedsService:
     def __init__(self, repository: FeedsRepository):
         self._repository = repository
+
+    async def exists_active_feed_user(self, chat_id: int, url: str) -> bool:
+        return await self._repository.exists_active_feed_user(chat_id=chat_id, url=url)
+
+    @staticmethod
+    async def validate_feed(url: str) -> bool:
+        try:
+            return bool((await run_in_threadpool(feedparser.parse, url)).get("entries"))
+        except URLError:
+            return False
+
+    async def add_feed(self, url: str, chat_id: int) -> None:
+        hash_url_chat_id = get_hash(url, chat_id)
+        await self._repository.add_feed(url, chat_id, hash_url_chat_id)
