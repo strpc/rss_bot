544936f90a941f90b1cb2c9a85ad067506933764
strpc <renat.kruchinskij@gmail.com>
2021-05-30 01:58:32 +0300

    добавил кеширование сервисных сообщений из базы

--- backend/app/core/service_messages/repository.py
+++ backend/app/core/service_messages/repository.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+from aiocache import Cache
+
 from app.core.clients.database import Database
 from app.core.service_messages.models import ServiceMessage
 
@@ -7,8 +9,22 @@ from app.core.service_messages.models import ServiceMessage
 class ServiceMessagesRepository:
     def __init__(self, database: Database):
         self._db = database
+        self._cache = Cache(Cache.MEMORY)
+        self._cache_ttl = 5
+
+    async def _get_message_from_cache(self, title: ServiceMessage) -> Optional[str]:
+        cached_result = await self._cache.get(title)
+        if cached_result is not None:
+            return cached_result
+
+    async def _set_cache_message(self, title: ServiceMessage, message: str) -> None:
+        await self._cache.set(key=title, value=message, ttl=self._cache_ttl)
 
     async def get_message(self, title: ServiceMessage) -> Optional[str]:
+        from_cache = await self._get_message_from_cache(title)
+        if from_cache is not None:
+            return from_cache
+
         query = """
         SELECT text
         FROM bot_service_message
@@ -16,4 +32,6 @@ class ServiceMessagesRepository:
         """
         row = await self._db.fetchone(query, (title,))
         if row is not None:
-            return row[0]  # type: ignore
+            message = row[0]  # type: ignore
+            await self._set_cache_message(title, message)
+            return message
