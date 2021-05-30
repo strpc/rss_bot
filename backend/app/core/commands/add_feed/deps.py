7a78587aa4eebed04f882c408afb7b31b1205527
strpc <renat.kruchinskij@gmail.com>
2021-05-30 21:59:45 +0300

    добавил прокидывние параметра лимита фидов

--- backend/app/core/commands/add_feed/deps.py
+++ backend/app/core/commands/add_feed/deps.py
@@ -1,6 +1,7 @@
 from fastapi import Depends
 
-from app.core.base_deps import get_telegram_client
+from app.config import MainConfig
+from app.core.base_deps import get_config, get_telegram_client
 from app.core.clients.telegram import Telegram
 from app.core.commands.add_feed.service import CommandAddFeedService
 from app.core.feeds.deps import get_feeds_service
@@ -13,9 +14,11 @@ def get_command_add_feed_service(
     feeds_service: FeedsService = Depends(get_feeds_service),
     telegram: Telegram = Depends(get_telegram_client),
     service_messages: ServiceMessagesService = Depends(get_service_messages_service),
+    config: MainConfig = Depends(get_config),
 ) -> CommandAddFeedService:
     return CommandAddFeedService(
         feeds_service=feeds_service,
         telegram=telegram,
         service_messages=service_messages,
+        limit_feed=config.app.limit_feed,
     )
