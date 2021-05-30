e56c3f6f7fe062d2ee46e743366a1c0a6fa8bbaf
strpc <renat.kruchinskij@gmail.com>
2021-05-30 03:52:46 +0300

    убрал настройку логера, актуализировал настройки

--- worker/app/main.py
+++ worker/app/main.py
@@ -1,37 +1,26 @@
-from app.project.settings import INTERVAL_BEAT_HOUR, INTERVAL_BEAT_MINUTE, RSS_BOT_BROKER
-from celery import Celery, signals
+from celery import Celery
 from celery.schedules import crontab
 
+from app.config import get_config
 
-app = Celery("rss_bot", broker=RSS_BOT_BROKER, timezone="UTC")
+
+config = get_config()
+
+app = Celery("rss_bot", broker=config.celery.broker, timezone="UTC")
 app.control.purge()  # !DEV
 app.autodiscover_tasks(
     [
-        "app.core.queues.tasks",
-    ]
+        "app.tasks",
+    ],
 )
 app.conf.task_default_queue = "rss_bot.download_articles_send_msg"
 app.conf.beat_schedule = {
     "download_articles_send_msg": {
-        "task": "src.core.queues.tasks.run_chain",
+        "task": "tasks.run_chain",
         "schedule": crontab(
-            hour=INTERVAL_BEAT_HOUR,
-            minute=INTERVAL_BEAT_MINUTE,
+            hour=config.celery.hour_beat_interval,
+            minute=config.celery.minute_beat_interval,
         ),
         "options": {"queue": "rss_bot.download_articles_send_msg"},
-    }
+    },
 }
-
-
-@signals.celeryd_init.connect
-def setup_log_format(sender, conf, **kwargs):
-    """Делаем форматирование лога celery"""
-    conf.worker_log_format = """
-        [%(asctime)s: %(levelname)s/%(processName)s {0}] %(module)s: %(funcName)s:  %(message)s
-    """.strip().format(
-        sender
-    )
-    conf.worker_task_log_format = (
-        "[%(asctime)s: %(levelname)s/%(processName)s {0}] "
-        "[%(task_name)s(%(task_id)s)] %(message)s"
-    ).format(sender)
