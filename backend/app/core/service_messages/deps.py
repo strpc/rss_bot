69fe70b7e99fb2faa02950424a21bb58c3101d74
strpc <renat.kruchinskij@gmail.com>
2021-05-30 21:31:04 +0300

    добавил прокидывание кеша вовнутрь репозитория

--- backend/app/core/service_messages/deps.py
+++ backend/app/core/service_messages/deps.py
@@ -1,6 +1,9 @@
+from typing import Type
+
+from aiocache import Cache
 from fastapi import Depends
 
-from app.core.base_deps import get_database, get_telegram_client
+from app.core.base_deps import get_cache_type, get_database, get_telegram_client
 from app.core.clients.database import Database
 from app.core.clients.telegram import Telegram
 from app.core.service_messages.repository import ServiceMessagesRepository
@@ -9,8 +12,9 @@ from app.core.service_messages.service import ServiceMessagesService
 
 def get_service_messages_repository(
     db: Database = Depends(get_database),
+    cache: Type[Cache] = Depends(get_cache_type),
 ) -> ServiceMessagesRepository:
-    return ServiceMessagesRepository(database=db)
+    return ServiceMessagesRepository(database=db, cache=cache)
 
 
 def get_service_messages_service(
