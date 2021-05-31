5fce6530e187e1339dcb171875944537454fbcae
strpc <renat.kruchinskij@gmail.com>
2021-06-01 00:14:08 +0300

    добавил сервис для отправки сообщения авторизации

---
+++ backend/app/core/commands/authorize/service.py
@@ -0,0 +1,19 @@
+from app.core.clients.telegram import Telegram
+from app.core.commands.command_abc import CommandServiceABC
+from app.core.service_messages.models import ServiceMessage
+from app.core.service_messages.service import ServiceMessagesService
+from app.schemas.message import Message
+
+
+class AuthorizeService(CommandServiceABC):
+    def __init__(
+        self,
+        *,
+        telegram: Telegram,
+        service_messages: ServiceMessagesService,
+    ):
+        self._telegram = telegram
+        self._service_messages = service_messages
+
+    async def handle(self, update: Message) -> None:
+        await self._service_messages.send(update.message.chat.id, ServiceMessage.authorize_message)
