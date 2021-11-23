870419272ef8aa7ae195c06a0daa1df5498eefe2
strpc <renat.kruchinskij@gmail.com>
2021-11-23 22:12:53 +0300

    feat: add default user in dev mode, load fixtures

--- admin/docker/entrypoint.sh
+++ admin/docker/entrypoint.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
 
-# !!!python ./app/manage.py loaddata --ignorenonexistent service_messages
-
-
 PORT=${RSS_BOT_PORT:-8001}
 LOG_LEVEL_UVICORN=${LOG_LEVEL_UVICORN:-info}
 
+DEV_USERNAME=user
+DEV_PASSWORD=hackme
+DEV_EMAIL=admin@admin.ru
+
+
+echo "Load fixtures service_messages..."
+python3 ./app/manage.py loaddata --ignorenonexistent service_messages
+
+
 # Apply database migrations
-echo "Apply database migrations"
+echo "Apply database migrations..."
 python3 ./app/manage.py migrate --no-input
 
 
@@ -16,6 +22,11 @@ python3 ./app/manage.py migrate --no-input
 echo "Starting server..."
 dev() {
   echo "DEV mode"
+  DJANGO_SUPERUSER_USERNAME=${DEV_USERNAME} \
+  DJANGO_SUPERUSER_PASSWORD=${DEV_PASSWORD} \
+  DJANGO_SUPERUSER_EMAIL=${DEV_EMAIL} python3 ./app/manage.py createsuperuser --no-input
+  echo "user: ${DEV_USERNAME} password: ${DEV_PASSWORD}"
+
   python3 ./app/manage.py runserver 0.0.0.0:${PORT}
 }
 
