ac00a28d804922c0d6d8997d48a04793f2a005de
strpc <renat.kruchinskij@gmail.com>
2021-11-24 09:42:34 +0300

    fix: schema for pocket button

--- backend/app/worker/send_entries_task/schemas.py
+++ backend/app/worker/send_entries_task/schemas.py
@@ -1,14 +1,24 @@
+from typing import Any, Dict, Union
+
+from pydantic import BaseModel, validator
+
 from app.api.endpoints.update.enums import ServiceIntegration
-from app.api.endpoints.update.schemas import Button, CallbackIntegrationServicePayload
+from app.api.endpoints.update.schemas import CallbackIntegrationServicePayload
 
 
-class PocketButton(Button):
-    service = ServiceIntegration.pocket
-    text = u"Save to Pocket \U0001F516"
+SAVE_POCKET_EMOJI = "\U0001F516"
 
-    def add_callback_data(self, entry_id: int) -> None:
-        self.callback_data = CallbackIntegrationServicePayload(
-            service=self.service,
-            entry_id=entry_id,
+
+class PocketButton(BaseModel):
+    service: ServiceIntegration = ServiceIntegration.pocket
+    text: str = u"Save to Pocket {EMOJI}".format(EMOJI=SAVE_POCKET_EMOJI)
+    callback_data: Union[str, int]
+
+    @validator("callback_data", pre=True, always=True)
+    def validate_callback_data(cls, field_value: int, values: Dict[str, Any], **kwargs: Any) -> str:
+        callback_data = CallbackIntegrationServicePayload(
+            service=values["service"],
+            entry_id=field_value,
             sended=False,
         ).json()
+        return callback_data
